41f44463ed96a04cdd038f8df7d7910fb7a55558
Daniel Brugada <danbruher@gmail.com>
2021-07-14 23:05:54 -0500

    Aprendi a usar la wea del plot :v

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 import io
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.set_option('deprecation.showPyplotGlobalUse', True)
 st.title('El proyecto de mineria')
 
 uploaded_file = st.file_uploader("Subir archivo CSV")
@@ -13,8 +15,8 @@ DatosMelbourne
 
 
 st.subheader('Paso 1: Descripcion de la estructura de loas datos')
-DatosMelbourne.shape
-DatosMelbourne.dtypes
+st.text(DatosMelbourne.shape)
+st.dataframe(DatosMelbourne.dtypes)
 
 st.subheader('Paso 2: Indentificaion de datos faltantes.')
 st.dataframe (DatosMelbourne.isnull().sum())
@@ -26,7 +28,7 @@ s = buffer.getvalue()
 st.write (s)
 
 st.subheader('Paso 3: Deteccion de valores atipicos')
-a = DatosMelbourne.hist(figsize=(15,15))
+DatosMelbourne.hist(figsize=(15,15))
 st.pyplot()
 
 st.dataframe(DatosMelbourne.describe())
@@ -34,8 +36,9 @@ st.dataframe(DatosMelbourne.describe())
 st.subheader('Paso 4: Identificacion de relaciones entre varialbles')
 ValoresAtipicos = ['Price', 'Landsize', 'BuildingArea','YearBuilt']
 for col in ValoresAtipicos:
-  sns.boxplot(col, data=DatosMelbourne)
-  st.pyplot()
+   fig, ax = plt.subplots()
+   ax = sns.boxplot(col, data=DatosMelbourne)
+   st.pyplot(fig)
 
 st.dataframe(DatosMelbourne.describe(include='object'))
 
